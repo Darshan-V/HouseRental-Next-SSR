68192e74ce7e83032e5e1df41fe39ed4d472c8cb
Darshan-V <mr.darshanv@gmail.com>
2024-07-16 22:46:35 +0530

    udpate and inster signup buttion in signin

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -5,7 +5,7 @@ const SignInPage: React.FC = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="max-w-md w-full px-6 py-8 bg-white shadow-md rounded-md">
         <h2 className="text-2xl font-semibold mb-6">Sign In</h2>
-        <form className="space-y-4">
+        <form className="space-y-4 w-full flex flex-col">
           <div>
             <label
               htmlFor="phone"
@@ -16,7 +16,9 @@ const SignInPage: React.FC = () => {
             <input
               type="tel"
               id="phone"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              inputMode="numeric"
+              pattern="[0-9]*"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-black sm:text-sm"
               placeholder="Enter your phone number"
             />
           </div>
@@ -30,7 +32,7 @@ const SignInPage: React.FC = () => {
             <input
               type="password"
               id="password"
-              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black"
               placeholder="Enter your password"
             />
           </div>
@@ -39,6 +41,12 @@ const SignInPage: React.FC = () => {
             className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Sign In
+          </button>
+          <button
+            type="button"
+            className="w-fit py-2 px-4 mx-auto border border-transparent rounded-md  text-sm font-medium text-black  hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 mt-4"
+          >
+            Sign Up
           </button>
         </form>
       </div>
