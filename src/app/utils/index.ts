4cbb70a985014dece72cdf397257b7157cd616ee
Darshan-V <mr.darshanv@gmail.com>
2024-07-23 20:30:29 +0530

    rename file that defines what's in it

--- src/app/utils/index.ts
+++
@@ -1,9 +0,0 @@
-const formatCurrency = (value: number) => {
-  // Implement your logic to format the currency
-  return value.toLocaleString("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-};
-
-export { formatCurrency };
