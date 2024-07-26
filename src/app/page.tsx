42e4127bc08e7c0dcee354a0462de7df4f7e0d78
Darshan-V <mr.darshanv@gmail.com>
2024-07-26 19:55:25 +0530

    add jsx layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,45 @@
-import Image from "next/image";
+"use client";
 import Navbar from "./components/Navbar";
+import ActionCard from "@/app/components/ActionCard";
+import {
+  BsPeopleFill,
+  BsFillPieChartFill,
+  BsSendExclamationFill,
+} from "react-icons/bs";
 
 export default function Home() {
-  const tenants = [
-    { id: 1, name: "John Doe", email: "john@example.com", phone: "1234567890" },
+  const mockData = [
     {
-      id: 2,
-      name: "Jane Smith",
-      email: "jane@example.com",
-      phone: "9876543210",
+      title: "Tenants",
+      description: "Description 1",
+      icon: <BsPeopleFill color="black" size="28" />,
     },
-    // Add more mock data here
+    {
+      title: "Statistics",
+      description: "Description 2",
+      icon: <BsFillPieChartFill color="black" size="28" />,
+    },
+    {
+      title: "Advance Returns",
+      description: "Description 3",
+      icon: <BsSendExclamationFill color="black" size="28" />,
+    },
   ];
 
   return (
     <div className="flex flex-col ">
       {/* Sidebar */}
-      <div className="sidebar">{/* Sidebar content */}</div>
-
+      <div className="sidebar">
+        {/* Sidebar content */}
+        <div
+          className="hamburger"
+          onClick={() => console.log("Sidebar Open/Close")}
+        >
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </div>
       {/* Main Content */}
       <div className="flex-grow">
         {/* Navbar */}
@@ -31,28 +53,17 @@ export default function Home() {
           />
         </div>
 
-        {/* Tenant Info Table */}
-        <div className="overflow-x-auto">
-          <table className="table-auto">
-            <thead>
-              <tr>
-                <th className="px-4 py-2">ID</th>
-                <th className="px-4 py-2">Name</th>
-                <th className="px-4 py-2">Email</th>
-                <th className="px-4 py-2">Phone</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tenants.map((tenant) => (
-                <tr key={tenant.id}>
-                  <td className="border px-4 py-2">{tenant.id}</td>
-                  <td className="border px-4 py-2">{tenant.name}</td>
-                  <td className="border px-4 py-2">{tenant.email}</td>
-                  <td className="border px-4 py-2">{tenant.phone}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        {/* Action Cards */}
+        <div className=" px-4  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+          {mockData.map((data, index) => (
+            <ActionCard
+              key={index}
+              title={data.title}
+              description={data.description}
+              icon={data.icon}
+              onClick={() => console.log(data.title)}
+            />
+          ))}
         </div>
       </div>
     </div>
