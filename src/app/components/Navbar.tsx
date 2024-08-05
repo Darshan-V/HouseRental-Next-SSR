e03d678859c5b56e6f373119120e723ba07a8cd6
Darshan-V <mr.darshanv@gmail.com>
2024-08-05 13:30:40 +0530

    explore and implement usePathname and implement it for navbar style

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { RiHomeSmileFill } from "react-icons/ri";
 
 interface LinkData {
@@ -12,16 +14,26 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ links }) => {
+  const pathName = usePathname();
+  console.log(pathName);
   return (
     <nav className=" w-full flex   bg-sky-500">
       <Link className="flex items-center p-2" href="/">
         <h1 className="text-2xl text-center ">SMB</h1>
         <RiHomeSmileFill size="28" />
       </Link>
-      <ul className="flex w-full flex-row justify-end gap-4 p-4">
+      <ul className="flex w-full flex-row justify-end gap-4 p-4  ">
         {links.map((link) => (
           <li key={link.href}>
-            <Link href={link.href}>{link.label}</Link>
+            <Link
+              href={link.href}
+              id={link.href}
+              className={`link ${
+                pathName === `/${link.href}` ? "text-black" : "text-white"
+              }`}
+            >
+              {link.label}
+            </Link>
           </li>
         ))}
       </ul>
