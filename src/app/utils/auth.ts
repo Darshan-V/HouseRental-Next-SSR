c557774ff48c246d4e9cd5bbc8e1022d2c83cc13
Darshan-V <mr.darshanv@gmail.com>
2024-12-03 10:07:42 +0530

    cleanup and init with new stack

--- src/app/utils/auth.ts
+++
@@ -1,8 +0,0 @@
-// src/utils/auth.ts
-export const login = async (email: string, password: string) => {
-  // Mock authentication
-  if (email === "test@example.com" && password === "password") {
-    return true;
-  }
-  return false;
-};
--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-interface RentPayment {
-  id: number;
-  dueDate: string;
-  expenses: number;
-  income: number;
-  status: string;
-}
-
-export type { RentPayment };
